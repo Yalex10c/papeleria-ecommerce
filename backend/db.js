0f9bec86742aff7ba4558000028222b150edbf98
Irvinglez12 <qoltra1413@gmail.com>
2025-06-10 00:28:24 -0600

    Se modifico   db.js para subirlo a la nube

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'ecommerce',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Necesario para Render y otros servicios que usan SSL auto-firmado
+  }
 });
 
 pool.connect()
